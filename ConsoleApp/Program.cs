893a4bd73b1265e90986bca216052adca2d19393
Niklas Lindberg <niklas.lindberg@allevi.se>
2024-10-09 10:22:41 +0200

    Pillat på Stefans class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Fonsan");
 Console.WriteLine("Niklas");
+Stefans_class stefans_Class = new Stefans_class();
